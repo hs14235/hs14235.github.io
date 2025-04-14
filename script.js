b5cdfe1962b58025721c64792506818b6656624b
hs14235 <hamexpired@gmail.com>
2025-04-13 23:45:12 -0400

    Adding experience & general information with transitions

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Portfolio loaded!");
+const faders = document.querySelectorAll('.fade-in');
+
+function revealOnScroll() {
+  const scrollY = window.scrollY;
+  const windowHeight = window.innerHeight;
+
+  faders.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < windowHeight - 50) {
+      section.classList.add('visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);

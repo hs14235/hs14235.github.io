2e89cd1067204fced8206ddf6b19454366006928
hs14235 <hamexpired@gmail.com>
2026-01-03 22:50:36 -0500

    changes so portfolio is mobile friendly

--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,45 @@
 (function ($) {
+  "use strict";
 
-    "use strict";
+  // PRE LOADER
+  $(window).on('load', function () {
+    $('.preloader').fadeOut(1000);
+  });
 
-        // PRE LOADER
-        $(window).load(function(){
-          $('.preloader').fadeOut(1000); // set duration in brackets    
-        });
+  // navigation Section
+  $('.navbar-collapse a').on('click', function () {
+    $(".navbar-collapse").collapse('hide');
+  });
 
+  // Parallax Js (desktop only)
+  function initParallax() {
+    if ($(window).width() > 992) {
+      $('#home').parallax("50%", 50);
+      $('#service').parallax("50%", 40);
+      $('#about').parallax("50%", 20);
+      $('#work').parallax("50%", 30);
+      $('#contact').parallax("50%", 10);
+    }
+  }
+  initParallax();
 
-        // navigation Section
-        $('.navbar-collapse a').on('click',function(){
-          $(".navbar-collapse").collapse('hide');
-        });
+  // smoothscroll js (all internal anchors)
+  $(function () {
+    $('a[href^="#"]').on('click', function (event) {
+      var target = $(this.getAttribute('href'));
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top - 49
+        }, 600);
+      }
+    });
+  });
 
-
-        // Parallax Js
-        function initParallax() {
-          $('#home').parallax("50%", 50);
-          $('#service').parallax("50%", 40);
-          $('#about').parallax("50%", 20);
-          $('#work').parallax("50%", 30);
-          $('#contact').parallax("50%", 10);
-          }
-        initParallax();
-        
-
-        // smoothscroll js
-        $(function() {
-          $('#home a').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 49
-            }, 1000);
-            event.preventDefault();
-          });
-        });  
-
-
-        // WOW Animation js
-        new WOW({ mobile: false }).init();
+  // WOW Animation js
+  new WOW({
+    mobile: true,
+    live: true
+  }).init();
 
 })(jQuery);
